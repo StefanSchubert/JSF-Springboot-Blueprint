40f749e4ec7640f60a13afc20813027008afe4a4
Schubert, Stefan <stefan.schubert@accso.de>
2021-05-08 10:42:05 +0200

    Show Error Page handling

--- src/main/java/de/bluewhale/controller/SampleView.java
+++ src/main/java/de/bluewhale/controller/SampleView.java
@@ -4,6 +4,8 @@ import de.bluewhale.CDIBeans.LocaleBean;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.context.annotation.RequestScope;
 
 import javax.inject.Named;
@@ -26,5 +28,20 @@ public class SampleView implements Serializable {
     public void businessCallViaViewAction() {
         log.info("Bussiness Logic of A called.");
     }
+
+    /**
+     * The purpose of this example is to show where to locate the static error page, so that the app can redirect to it
+     * if something like this happen
+     */
+    public void simulateBackendError()  {
+        log.error("Outch - backend got a severe problem.");
+        // throw new HttpTimeoutException("Simulate Timeout");
+        throw new HttpServerErrorException(HttpStatus.INTERNAL_SERVER_ERROR,"Backend problem");
+    }
+
+    public String simulate404Error()  {
+        return "ThisOutComeDOesNotExists";
+    }
+
 
 }
